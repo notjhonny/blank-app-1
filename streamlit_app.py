1136777c8cefd759c2a65f218ead531819f6ae6d
notjhonny <167467885+notjhonny@users.noreply.github.com>
2025-03-17 17:50:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,52 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("teste")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "teste streamlit nodes"
+
+    from streamlit_flow import streamlit_flow
+from streamlit_flow.elements import StreamlitFlowNode, StreamlitFlowEdge
+from streamlit_flow.state import StreamlitFlowState
+
+nodes = [StreamlitFlowNode( id='1', 
+                            pos=(100, 100), 
+                            data={'content': 'Node 1'}, 
+                            node_type='input', 
+                            source_position='right', 
+                            draggable=False),
+        StreamlitFlowNode(  '2',
+                            (350, 50), 
+                            {'content': 'Node 2'}, 
+                            'default', 
+                            'right', 
+                            'left', 
+                            draggable=False),
+        StreamlitFlowNode(  '3', 
+                            (350, 150), 
+                            {'content': 'Node 3'}, 
+                            'default', 
+                            'right', 
+                            'left', 
+                            draggable=False),
+        StreamlitFlowNode(  '4', 
+                            (600, 100), 
+                            {'content': 'Node 4'}, 
+                            'output', 
+                            target_position='left', 
+                            draggable=False)]
+
+edges = [StreamlitFlowEdge('1-2', '1', '2', animated=True, marker_end={'type': 'arrow'}),
+        StreamlitFlowEdge('1-3', '1', '3', animated=True, marker_end={'type': 'arrow'}),
+        StreamlitFlowEdge('2-4', '2', '4', animated=True, marker_end={'type': 'arrow'}),
+        StreamlitFlowEdge('3-4', '3', '4', animated=True, marker_end={'type': 'arrow'})]
+
+state = StreamlitFlowState(nodes, edges)
+
+streamlit_flow('static_flow',
+                state,
+                fit_view=True,
+                show_minimap=False,
+                show_controls=False,
+                pan_on_drag=False,
+                allow_zoom=False)
 )
